1b27c118b54d07af8f33c053c5ca3b672a5c6837
emon1432 <ovirajemon143@gmail.com>
2025-04-19 00:43:47 +0600

    Refactor dashboard route and redirect home to dashboard
    - Updated the home route to redirect to the dashboard instead of returning the welcome view.
    - Refactored the dashboard route to use a controller (DashboardController) for better organization and maintainability.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,60 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en" class="layout-navbar-fixed layout-compact layout-menu-fixed" dir="ltr" data-skin="default"
+    data-assets-path="/assets/" data-template="vertical-menu-template" data-bs-theme="light">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="robots" content="noindex">
+    <title>Demo: Dashboard - Analytics | Vuexy - Bootstrap Dashboard PRO</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets') }}/img/favicon/favicon.ico">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;ampdisplay=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/fonts/iconify-icons.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/libs/node-waves/node-waves.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/libs/pickr/pickr-themes.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/css/core.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/demo.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/fonts/flag-icons.css">
+    <link rel="stylesheet" href="{{ asset('assets') }}/vendor/css/pages/cards-advance.css">
+    <script src="{{ asset('assets') }}/vendor/js/helpers.js"></script>
+    <script src="{{ asset('assets') }}/vendor/js/template-customizer.js"></script>
+    <script src="{{ asset('assets') }}/js/config.js"></script>
+    @stack('styles')
+</head>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <x-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
+<body style="--bs-scrollbar-width: 15px;">
+    <div class="layout-wrapper layout-content-navbar  ">
+        <div class="layout-container">
+            @include('layouts.includes.sidebar')
+            <div class="layout-page">
+                @include('layouts.includes.navbar')
+                <div class="content-wrapper">
+                    <div class="container-fluid flex-grow-1 container-p-y">
+                        <div class="row g-6">
+                            @yield('content')
+                        </div>
                     </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                    @include('layouts.includes.footer')
+                </div>
+            </div>
         </div>
+        @include('layouts.includes.layout-menu-toggle')
+    </div>
+    <script src="{{ asset('assets') }}/vendor/libs/jquery/jquery.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/popper/popper.js"></script>
+    <script src="{{ asset('assets') }}/vendor/js/bootstrap.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/node-waves/node-waves.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/@algolia/autocomplete-js.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/pickr/pickr.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/i18n/i18n.js"></script>
+    <script src="{{ asset('assets') }}/vendor/js/menu.js"></script>
+    <script src="{{ asset('assets') }}/js/main.js"></script>
+    @stack('scripts')
+</body>
 
-        @stack('modals')
-
-        @livewireScripts
-    </body>
 </html>
---
+++ resources/views/layouts/includes/layout-menu-toggle.blade.php
@@ -0,0 +1,4 @@
+<div class="layout-overlay layout-menu-toggle"></div>
+<div class="drag-target"
+    style="touch-action: pan-y; user-select: none; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+</div>
