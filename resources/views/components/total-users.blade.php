864301eb83773b2544db54bfaa145221d06c2891
emon1432 <ovirajemon143@gmail.com>
2025-05-15 17:42:50 +0600

    fix: add missing data-popup attribute for tooltip customization in user avatar list

--- resources/views/components/total-users.blade.php
+++ resources/views/components/total-users.blade.php
@@ -1,11 +1,10 @@
 <ul class="list-unstyled d-flex align-items-center avatar-group mb-0">
     @foreach ($users as $user)
-        <li data-bs-toggle="tooltip"  data-bs-placement="top" title="{{ $user->name }}"
+        <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" title="{{ $user->name }}"
             class="avatar pull-up">
             <img class="rounded-circle" src="{{ imageShow($user->image) }}" alt="{{ $user->name }}" />
         </li>
     @endforeach
-
     @if ($moreCount > 0)
         <li class="avatar">
             <span class="avatar-initial rounded-circle pull-up" data-bs-toggle="tooltip" data-bs-placement="bottom"
