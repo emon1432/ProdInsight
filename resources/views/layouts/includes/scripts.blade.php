e63c497a3d55cd4e243510def957d2dd04d6bad5
emon1432 <ovirajemon143@gmail.com>
2025-05-24 12:06:58 +0600

    feat: initialize Bootstrap tooltips on DataTable draw callback

--- resources/views/layouts/includes/scripts.blade.php
+++ resources/views/layouts/includes/scripts.blade.php
@@ -160,6 +160,13 @@
                             return wrapper;
                         }
                     }
+                },
+                drawCallback: function() {
+                    const tooltipTriggerList = [].slice.call(document.querySelectorAll(
+                        '[data-bs-toggle="tooltip"]'));
+                    tooltipTriggerList.map(function(tooltipTriggerEl) {
+                        return new bootstrap.Tooltip(tooltipTriggerEl);
+                    });
                 }
             });
 
