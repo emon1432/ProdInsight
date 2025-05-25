ce38bfb63ddf8110cc14446886563b8bbf420834
emon1432 <ovirajemon143@gmail.com>
2025-05-25 15:11:25 +0600

    feat: implement permission checks for user and role management actions

--- resources/views/pages/roles-permissions/index.blade.php
+++ resources/views/pages/roles-permissions/index.blade.php
@@ -3,12 +3,14 @@
     <div class="card">
         <div class="card-header border-bottom d-flex justify-content-between align-items-center">
             <h5 class="card-title mb-0">{{ __('Roles & Permissions') }}</h5>
-            <a class="btn add-new btn-primary" href="{{ route('roles-permissions.create') }}">
-                <span class="d-flex align-items-center gap-2 text-white">
-                    <i class="icon-base ti tabler-plus icon-xs"></i>
-                    {{ __('Add New Record') }}
-                </span>
-            </a>
+            @if (check_permission('roles-permissions.create'))
+                <a class="btn add-new btn-primary" href="{{ route('roles-permissions.create') }}">
+                    <span class="d-flex align-items-center gap-2 text-white">
+                        <i class="icon-base ti tabler-plus icon-xs"></i>
+                        {{ __('Add New Record') }}
+                    </span>
+                </a>
+            @endif
         </div>
         <div class="card-datatable">
             <table class="common-datatable table d-table" data-url="{{ route('roles-permissions.index') }}"
@@ -17,7 +19,7 @@
                 { "data": "total_users" },
                  @if (check_permission('roles-permissions.show')) { "data": "permission_view" }, @endif
                 { "data": "status" },
-                { "data": "actions"}
+                 @if (main_menu_permission('roles-permissions')) { "data": "actions" } @endif
                 ]'>
                 <thead class="border-top">
                     <tr>
@@ -27,7 +29,9 @@
                             <th>{{ __('Permissions') }}</th>
                         @endif
                         <th>{{ __('Status') }}</th>
-                        <th>{{ __('Action') }}</th>
+                        @if (main_menu_permission('roles-permissions'))
+                            <th>{{ __('Actions') }}</th>
+                        @endif
                     </tr>
                 </thead>
             </table>
